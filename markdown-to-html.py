600d077f4df7b0a6272508a99188392deb75df47
Joni Laakso <v4loch@pop-os.localdomain>
2022-08-12 21:17:41 +0300

    add messagebox

--- markdown-to-html.py
+++ markdown-to-html.py
@@ -1,54 +1,81 @@
+from turtle import color
 import markdown
 from tkinter import *
 from tkinter import StringVar, ttk, filedialog
+from tkinter import messagebox as mbox
 
 #GLOBAL VARIABLES: 
 
 fileInPath= ""
 fileOutPath= ""
+fileLocation = ""
 
 
 # FUNCTIONS
 
 ## Open file
 def inputFile():
-    filename_in = filedialog.askopenfilename(initialdir="./", title = "Select .md file",
+    statusLabel.configure(text="")
+    dialogFileIn = filedialog.askopenfilename(initialdir="./", title = "Select .md file",
                                             filetypes=(("Markdown files", "*.md"), ("all files", "*.*")))
 
-    inOpenedLabel.configure(text=filename_in)
+    inOpenedLabel.configure(text=dialogFileIn)
 
-    print(filename_in)
+    print(dialogFileIn)
 
     global fileInPath  
-    fileInPath = str(filename_in)
-
-    
+    fileInPath = "/"+str(dialogFileIn)
     return 
 
 ## Output file
 def outputFile():
-    filename_out = filedialog.asksaveasfile(initialdir="./", title="Select where to save file",
+    statusLabel.configure(text="")
+
+    dialogFileOut = filedialog.asksaveasfile(initialdir="./", title="Select where to save file",
                                             filetypes=(("Html files", "*.html"), ("all files", "*.*")))
     
-    outOpenedLabel.configure(text=filename_out.name)
+    outOpenedLabel.configure(text=dialogFileOut.name)
 
     global fileOutPath 
-    fileOutPath = filename_out.name
+    fileOutPath = dialogFileOut.name
     
 
 #Convert file 
 def convertFile(inFilename, outFilename):
 
-        ##Open input file
-    with open(inFilename, 'r') as input_file:
-        text = input_file.read()
+    global fileInPath
+    global fileOutPath
+    global fileLocation
 
-    html = markdown.markdown(text)
-    print(html)
+    if inFilename == "" or outFilename == "":
+        print("Input or output file missing!")
+        statusLabel.configure(text="Input or output file missing!", fg="red")
 
-    with open(outFilename, 'w') as output_file:
-        output_file.write(html)
-    print("file converted!")
+    else:
+            ##Open input file
+        with open(inFilename, 'r') as input_file:
+            text = input_file.read()
+
+        html = markdown.markdown(text)
+        print(html)
+
+        with open(outFilename, 'w') as output_file:
+            output_file.write(html)
+        print("file converted!")
+        statusLabel.configure(text="File Converted to HTML", fg="black")
+        
+        fileLocation = fileOutPath
+        fileInPath = ""
+        fileOutPath = ""
+        inOpenedLabel.configure(text="")
+        outOpenedLabel.configure(text="")
+        filePopup()
+
+#File convert popup
+
+def filePopup():
+    global fileLocation
+    mbox.showinfo(title="File convert Success!", message="You have converted file: " + fileLocation)
 
 
 
@@ -57,16 +84,16 @@ def convertFile(inFilename, outFilename):
 #Create root ikkuna
 root = Tk()
 root.title("Md to HTML")
-root.geometry('600x200')
+root.geometry('800x200')
 root.resizable(0,0)
 
 #Configure grid:
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.columnconfigure(2, weight=1)
-root.rowconfigure(0, weight=0)
-root.rowconfigure(1, weight=0)
-root.rowconfigure(2, weight=2)
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=3)
 
 ##INPUT FILE
 #File path 
@@ -84,6 +111,10 @@ outFilepathLabel.grid(column=0, row=1, sticky=W, padx=10, pady=5)
 ## output filepath (opened)
 outOpenedLabel= Label(root, text="")
 outOpenedLabel.grid(column=1, row=1, sticky=W)
+
+## Status label
+statusLabel = Label(root, text="")
+statusLabel.grid(column=1, row=3, sticky=S)
 
 #Buttons:
 
@@ -112,7 +143,7 @@ buttonConvertFile = Button(root,
                             command=lambda: convertFile(fileInPath, fileOutPath),
                             width=40,
                             height=2)
-buttonConvertFile.grid(column=1, row=2)
+buttonConvertFile.grid(column=1, row=2, sticky=S)
 
 
 
